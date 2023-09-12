3721b9a099980641fcf2bc86fabec4a070187be6
Yael Méndez <yael.mendez@inflexionsoftware.com>
2023-09-12 16:43:46 -0600

    Login terminado
    Se termina el frontend de la pantalla del login.

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-screen.component.scss']
 })
 export class LoginScreenComponent implements OnInit {
+  //Aquí se definen las variables
+  public type: String = "password";
+  public username: String = "";
+  public password: String = "";
 
-  constructor() { }
+  public errors:any = {};
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-}
+  public login(){
+    if(this.username == ""){
+      this.errors.username = "Campo requerido";
+    }
+    if(this.password == ""){
+      this.errors.password = "Campo requerido";
+    }
+  }
+
+  public showPassword(){
+    if(this.type == "password"){
+      this.type = "text";
+    }else{
+      this.type = "password";
+    }
+  }
+
+  public goRegistro(){
+    this.router.navigate(["registro"]);
+  }
+
+}//Fin clase
