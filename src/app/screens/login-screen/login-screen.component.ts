d386601c080de94c90cce64a8c7420c3246abc72
Jovas Solis <jovany.solis.ortiz.18.04@gmail.com>
2023-09-15 20:32:48 -0600

    Creacion de Registro y new panel
    corroborar campos requeridos y mostrar nueva screen

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.component.html',
-  styleUrls: ['./login-screen.component.scss']
+  styleUrls: ['./login-screen.component.scss'],
 })
 export class LoginScreenComponent implements OnInit {
-///DEFINIENDO VARIABLES
-  public type: string="";
+  //Aquí se definen las variables
+  public type: String = 'password';
+  public username: String = '';
+  public password: String = '';
 
-  constructor() { }
+  public errors: any = {};
 
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  public login() {
+    if (this.username == '') {
+      this.errors.username = 'Campo requerido';
+    }
+    if (this.password == '') {
+      this.errors.password = 'Campo requerido';
+    }
   }
-public login (){
-  console.log("login");
-}
 
-}
-1
+  public showPassword() {
+    if (this.type == 'password') {
+      this.type = 'text';
+    } else {
+      this.type = 'password';
+    }
+  }
+
+  public goRegistro() {
+    this.router.navigate(['registro']);
+  }
+} 
