fc6585cbf0a070454090a88a5d4a8d56b7449fa3
Jovas Solis <jovany.solis.ortiz.18.04@gmail.com>
2023-09-27 21:28:15 -0600

    validacion extra de id
    vacia id si no es lo que se pide

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -20,47 +20,40 @@ export class ProductosService {
     };
   }
 
-  //Función para validar datos del producto
   public validarProducto(data: any) {
     console.log('Validando producto... ', data);
-    let error: any = [];
+    let error: any = {};
 
     if (!this.validatorService.required(data['id'])) {
       error['id'] = this.errorService.required;
     } else if (!this.validatorService.numeric(data['id'])) {
-      alert('El formato de ID es solo números');
+      error['id'] = 'El formato de ID es solo números';
     }
 
     if (!this.validatorService.required(data['nombre'])) {
       error['nombre'] = this.errorService.required;
     } else if (!this.validatorService.min(data['nombre'], 3)) {
-      error['nombre'] = this.errorService.min(3);
-      alert('La longitud de caracteres del Nombre es menor, deben ser 3 Minimo');
+      error['nombre'] =
+        'La longitud de caracteres del Nombre es menor, deben ser 3 Minimo';
     } else if (!this.validatorService.max(data['nombre'], 20)) {
-      error['nombre'] = this.errorService.max(20);
-      alert(
-        'La longitud de caracteres del Nombre del producto deben ser Máximo 20'
-      );
+      error['nombre'] =
+        'La longitud de caracteres del Nombre del producto deben ser Máximo 20';
     }
 
     if (!this.validatorService.required(data['departamento'])) {
       error['departamento'] = this.errorService.required;
     } else if (!this.validatorService.min(data['departamento'], 3)) {
-      error['departamento'] = this.errorService.min(3);
-      alert(
-        'La longitud de caracteres de la departamento es menor, deben ser 3 Minimo'
-      );
+      error['departamento'] =
+        'La longitud de caracteres de la departamento es menor, deben ser 3 Minimo';
     } else if (!this.validatorService.max(data['nombre'], 20)) {
-      error['departamento'] = this.errorService.max(20);
-      alert(
-        'La longitud de caracteres del departamento deben ser Máximo 20'
-      );
+      error['departamento'] =
+        'La longitud de caracteres del departamento deben ser Máximo 20';
     }
 
     if (!this.validatorService.required(data['precio'])) {
       error['precio'] = this.errorService.required;
     } else if (!this.validatorService.numeric(data['precio'])) {
-      alert('El formato de Precio es solo números');
+      error['precio'] = 'El formato de Precio es solo números';
     }
 
     return error;
